4bb2d1676ec464cf45b136dbb19b7ec9da54e715
Miami <alex.miami.petrov@gmail.com>
2024-05-28 13:50:10 +0300

    m

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = (users) => {
-  return users.sort(
+  return users.toSorted(
     (firstUser, secondUser) =>
       secondUser.friends.length - firstUser.friends.length
   );
